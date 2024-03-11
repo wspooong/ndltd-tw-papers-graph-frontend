758d92b48f80dc7d2d202a9a0df9042f0efcf843
wspooong <wspooong@gmail.com>
2024-03-11 22:23:33 +0800

    Add graphLayoutSize store with setBrowserWindowSize action

---
+++ src/stores/graphLayoutSize.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+
+export const useGraphLayoutSizeStore = defineStore('graphLayoutSize', {
+    state: () => ({ 
+        width: 0,
+        height: 0,
+        margin: 150
+     }),
+    actions: {
+        setBrowserWindowSize(width: number, height: number) {
+            this.width = width-this.margin;
+            this.height = height-this.margin;
+        },
+    }
+})

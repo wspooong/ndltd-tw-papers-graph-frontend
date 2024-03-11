97443e867e02d36ae203014ed29e16b7b32f2d5b
wspooong <wspooong@gmail.com>
2024-03-11 22:22:53 +0800

    Add ErrorLayout.vue for handling 404 errors

---
+++ src/layouts/ErrorLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container fluid class="text-center bg-grey-lighten-3 fill-height">
+    <v-responsive>
+      <v-row class="pa-0">
+        <v-col cols="12" md="6" offset-md="3">
+          <div style="font-size: 15vh">404</div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6" offset-md="3">
+          <iframe src="https://giphy.com/embed/EXnkKLO2XWu8CJEZUm" width="480" height="330" frameBorder="0"
+            class="giphy-embed" style="pointer-events: none;">
+          </iframe>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6" offset-md="3">
+          <div class="text-h2">
+            看來你走錯地方了...
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" md="6" offset-md="3">
+          <v-btn href="/" color="primary" large>
+            回到首頁
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-responsive>
+  </v-container>
+</template>
+  

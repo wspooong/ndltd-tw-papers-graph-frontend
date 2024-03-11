3f1f95655047c7f4c76e5fc4409f33688638f892
wspooong <wspooong@gmail.com>
2024-03-11 22:20:03 +0800

    Add HeaderLink component

---
+++ src/components/HeaderLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-list-item :href="link" target="_blank" clickable>
+    <v-list-item>
+      <v-list-item-title>{{ title }}</v-list-item-title>
+      <v-list-item-subtitle>{{ caption }}</v-list-item-subtitle>
+    </v-list-item>
+  </v-list-item>
+</template>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue';
+
+export interface HeaderLinkProps {
+  title: string;
+  caption?: string;
+  link?: string;
+}
+
+withDefaults(defineProps<HeaderLinkProps>(), {
+  caption: '',
+  link: '#',
+});
+</script>

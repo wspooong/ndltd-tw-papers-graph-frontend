fffd0810014a5eb0f69b1e7bb073260174663e66
wspooong <wspooong@gmail.com>
2024-04-10 21:47:22 +0800

    Add VueGtagPlugin for Google Analytics tracking

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ import router from './router'
 // Composables
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueGtagPlugin from 'vue-gtag'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -23,4 +24,8 @@ registerPlugins(app)
 
 app.use(router)
 app.use(pinia)
+app.use(VueGtagPlugin, {
+    config: { id: 'G-JR8F0BXDB8' }
+    }, router
+)
 app.mount('#app')
